639a8faabcb07f0cb7365f579d699389213ab7f6
vaishnavsulakhe <vaishnavkumarvaishu@gmail.com>
2023-10-07 12:22:19 +0530

    second commit

---
+++ project1/hello.cpp
@@ -0,0 +1,48 @@
+// Your First C++ Program
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello World!";
+    return 0;
+}
+
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+hello.cpp [unix] (05:29 01/01/1970)                                                                                           0,0-1 All
+"hello.cpp" [New]
+
